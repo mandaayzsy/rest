03fb04ca2514e837271a22a76bec3fc349af33be
mandaayzsy <mandaayzsy@gmail.com>
2024-06-10 22:45:38 +0700

    first commit

---
+++ getImage.php
@@ -0,0 +1,53 @@
+<?php
+	// Parameter check
+	if (!isset($_GET['name']) || !isset($_GET['size'])) {
+		echo 'Error: Name and size required.';
+		exit();
+	}
+	
+	// Construct the image path and file name based on parameters
+	$filename = './images-' . $_GET['size'] . '/' . $_GET['name'] . '.png';
+	
+	if (file_exists($filename)) {
+		// If the file exists, just return the file with appropriate header
+		header("Content-Type: image/png");
+		readfile($filename);
+	} else {
+		// If it doesn't exist, read our base image first
+		$source = './images-xxxhdpi/' . $_GET['name'] . '.png';
+		if (!file_exists($source)) {
+			echo 'Error: Source image not found.';
+			exit();
+		}
+		$image = imagecreatefrompng($source);
+
+		// Then find target image size
+		$pixels = array(88, 132, 176, 264, 352);
+		$sizes = array('mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi');
+		$i = array_search($_GET['size'], $sizes);
+		
+		if ($i === false) {
+			echo 'Error: Invalid size parameter.';
+			exit();
+		}
+		
+		// Here we resize the image
+		$result = imagecreatetruecolor($pixels[$i], $pixels[$i]);
+		imagealphablending($result, false);
+		imagesavealpha($result, true);
+		$transparent = imagecolorallocatealpha($result, 255, 255, 255, 127);
+		imagefilledrectangle($result, 0, 0, $pixels[$i], $pixels[$i], $transparent);
+		imagecopyresampled($result, $image, 0, 0, 0, 0, $pixels[$i], $pixels[$i], $pixels[4], $pixels[4]);
+
+		// Save it as cache
+		imagepng($result, $filename);
+		
+		// Output it
+		header("Content-Type: image/png");
+		imagepng($result);
+		
+		// Finally, free images from memory
+		imagedestroy($image);
+		imagedestroy($result);
+	}
+?>
